bcdb64f83af13b465a1e920b3a38f844eb79082c
Y-rtn <1650673710@qq.com>
2024-08-05 16:24:49 +0800

    接口调用

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,18 @@
 // src/components/TodoList.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
+import axios from 'axios';
 import todoStore from '../stores/TodoStore';
 const Button = React.lazy(() => import('compoKit/Button')); // 引入 CompoKit 组件
 
 const TodoList = observer(() => {
+  const [data, setData] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3002/api')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
   return (
     <div>
       <h1>Todo List</h1>
@@ -19,6 +27,7 @@ const TodoList = observer(() => {
         onChange={(e) => todoStore.setNewTodo(e.target.value)}
       />
       <React.Suspense fallback={<div>Loading...</div>}>
+        {data}
         <Button onClick={() => todoStore.addTodo()}>Add Todo</Button>
       </React.Suspense>
     </div>
